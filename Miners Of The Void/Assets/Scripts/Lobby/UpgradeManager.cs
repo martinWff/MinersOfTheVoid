33f3712cc6934a2d75b56188eba4c0181db2bf55
miguel81655 <migueldleandro@gmail.com>
2021-11-10 13:29:08 +0000

    Upgrades Fixed And Done
    Lobby upgrades finished completly.

--- Miners Of The Void/Assets/Scripts/Lobby/UpgradeManager.cs
+++ Miners Of The Void/Assets/Scripts/Lobby/UpgradeManager.cs
@@ -20,6 +20,7 @@ namespace MOV.Upgrades
         public GameObject slot4;
         public GameObject insertGO;
         private bool imageIn = false;
+        private bool full = false;
 
 
 
@@ -40,6 +41,38 @@ namespace MOV.Upgrades
         public void ASummon()
         {
             
+                if (!imageIn)
+                {
+                    if (slot1.transform.childCount == 0)
+                    {
+                        Instantiate(insertGO, slot1.transform);
+                        full = false;
+                    }
+                    else if (slot2.transform.childCount == 0)
+                    {
+                        Instantiate(insertGO, slot2.transform);
+                        full = false;
+                    }
+                    else if (slot3.transform.childCount == 0)
+                    {
+                        Instantiate(insertGO, slot3.transform);
+                        full = false;
+                    }
+                    else if (slot4.transform.childCount == 0)
+                    {
+                        Instantiate(insertGO, slot4.transform);
+                        full = false;
+                        Debug.Log(full);
+                    }
+                    else
+                    {
+                        Debug.Log(full);
+                        full = true;
+                    }
+                    imageIn = true;
+                }
+            if (full == false)
+            {
                 if (upgradeType == "SpeedButton")
                 {
                     level = playerstats.speedLevel;
@@ -77,30 +110,7 @@ namespace MOV.Upgrades
                 //mySlots.InsertAtEnd("backWeapon");
             }
 
-            if (!imageIn)
-            {
-                if (slot1.transform.childCount == 0)
-                {
-                    Instantiate(insertGO, slot1.transform);
-                }
-                else if (slot2.transform.childCount == 0)
-                {
-                    Instantiate(insertGO, slot2.transform);
-                }
-                else if (slot3.transform.childCount == 0)
-                {
-                    Instantiate(insertGO, slot3.transform);
-                }
-                else if (slot4.transform.childCount == 0)
-                {
-                    Instantiate(insertGO, slot4.transform);
-                }
-                else
-                {
-                   
-                }
                 
-                imageIn = true;
             }
             /*Debug.Log(mySlots.Get(0));
             Debug.Log(mySlots.Get(1));
@@ -109,6 +119,7 @@ namespace MOV.Upgrades
         }
         public void UnSummon()
         {
+            
             
             if (upgradeType == "SpeedButton")
             {
