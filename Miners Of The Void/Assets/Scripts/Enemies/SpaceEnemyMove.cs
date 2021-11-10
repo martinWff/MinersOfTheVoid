b8a7a259d070eb37bc487bf0c9bf3f204e1c8f3c
HugoBonito <92577431+HugoBonito@users.noreply.github.com>
2021-11-10 14:56:44 +0000

    PirateNormal
    PirateNormal prefab add:
    movement
    
    new prefab PirateExplosion

--- Miners Of The Void/Assets/Scripts/Enemies/SpaceEnemyMove.cs
+++ Miners Of The Void/Assets/Scripts/Enemies/SpaceEnemyMove.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class SpaceEnemyMove : MonoBehaviour
 {
     private GameObject spaceship;
+    private float distance;
+    private Rigidbody2D enemy;
+    public float enemyRange = 30;
+    public float speed = 3;
+    private Vector3 targetPosition;
 
+    public float nearPlayer = 15;
 
     // Start is called before the first frame update
     void Start()
     {
-        spaceship = GameObject.FindGameObjectWithTag("Sapceship");
+        spaceship = GameObject.FindGameObjectWithTag("Spaceship");
+        enemy = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPosition = new Vector3(spaceship.transform.position.x, spaceship.transform.position.y, spaceship.transform.position.z);
+        Vector3 direction = spaceship.transform.position - transform.position;
+        targetPosition = playerPosition;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        enemy.rotation = angle - 90;
+
+        distance = Mathf.Sqrt(Mathf.Pow(spaceship.transform.position.x - transform.position.x, 2) + Mathf.Pow(spaceship.transform.position.y - transform.position.y, 2));
+        if (distance < enemyRange)
+        {
+            if(distance > nearPlayer)
+            {
+                //Debug.Log("speed: " + transform.position);
+               transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            }
+            
+
+        }
     }
 }
