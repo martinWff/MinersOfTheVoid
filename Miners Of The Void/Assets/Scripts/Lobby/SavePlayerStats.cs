d2757768f0be5cc5b33ae8429c0aa7c4b45669bf
miguel81655 <migueldleandro@gmail.com>
2021-11-30 12:37:18 +0000

    Human Player fix and upgrades
    Added life bar to human player and added a text that shows the costs of the upgrades when you can't buy them.

--- Miners Of The Void/Assets/Scripts/Lobby/SavePlayerStats.cs
+++ Miners Of The Void/Assets/Scripts/Lobby/SavePlayerStats.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public static class SavePlayerStats 
 {
+    //Spaceship
     public static float playerDamage = 10;
     public static float totalShield = 10;
     public static float shield = 10;
@@ -16,6 +17,23 @@ public static class SavePlayerStats
     public static float dmgLevel = 0;
     public static float shieldLevel = 0;
     public static float healthLevel = 0;
+
+    //HumanPlayer
+    public static float playerDamageH = 10;
+    public static float totalShieldH = 10;
+    public static float shieldH = 10;
+    public static float hpH = 20;
+    public static float totalhpH = 20;
+    public static float moveForceH = 4;
+    public static float bulletSpeedH = 14;
+    public static bool backWeaponH = false;
+    public static float speedLevelH = 0;
+    public static float dmgLevelH = 0;
+    public static float shieldLevelH = 0;
+    public static float healthLevelH = 0;
+
+
+
     //Money
     public static int bips = 1000000000;
 }
