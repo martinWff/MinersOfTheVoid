4159910aa820c7813a79f40b747c9c1011cd2642
HugoBonito <92577431+HugoBonito@users.noreply.github.com>
2021-12-26 16:31:32 +0000

    creating the shockwave

--- Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI.cs
+++ Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class phaseI : MonoBehaviour
 {
+    [SerializeField] public GameObject shockWave;
 
+    private Rigidbody2D boss;
+
+    //shoot
+    public float bulletOffset = 2;
+    public float bulletShootTime = 1.5f;
+    public float bulletCooldownTime = 1.5f;
+    public float bulletSpeed = 14;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +24,21 @@ public class phaseI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void ShotShockWave()
+    {
+
+        Vector3 Shotdirection = new Vector3(0, -1, 0);
+        if ((bulletShootTime <= 0))
+        {
+            GameObject shockwave = Instantiate(shockWave, transform.position + (Shotdirection.normalized * bulletOffset), Quaternion.identity);
+            bulletShootTime = bulletCooldownTime;
+            shockwave.GetComponent<Rigidbody2D>().velocity = Shotdirection.normalized * bulletSpeed;
+        }
+        if (bulletShootTime >= 0)
+        {
+            bulletShootTime -= Time.deltaTime;
+        }
     }
 }
