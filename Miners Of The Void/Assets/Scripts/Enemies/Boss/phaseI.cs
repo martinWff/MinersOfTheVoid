1ce71e1fbe49b58284dfa9b82eb2ca330d9c2b27
HugoBonito <92577431+HugoBonito@users.noreply.github.com>
2022-01-02 21:41:42 +0000

    phaseI boss almost done

--- Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI.cs
+++ Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI.cs
@@ -7,7 +7,10 @@ public class phaseI : MonoBehaviour
     [SerializeField] public GameObject shockWave;
     [SerializeField] public GameObject safeArea;
 
+    //Boss
     private Rigidbody2D boss;
+    private GameObject bossG;
+    private float bossSize;
 
     //shoot
     public float shootTimer = 0;
@@ -26,12 +29,26 @@ public class phaseI : MonoBehaviour
     private float safeAreaRadius;
     private int ramdomNumberX;
     private int ramdomNumberY;
+    private bool spawnAreaBool = false;
+    
 
     // rect1 spawn safe area
     private float rect1moreX;
     private float rect1lessX;
     private float rect1moreY;
     private float rect1lessY;
+
+    // rect2 spawn safe area
+    private float rect2moreX;
+    private float rect2lessX;
+    private float rect2moreY;
+    private float rect2lessY;
+
+    // rect3 spawn safe area
+    private float rect3moreX;
+    private float rect3lessX;
+    private float rect3moreY;
+    private float rect3lessY;
 
 
     //Camera Componets
@@ -50,21 +67,25 @@ public class phaseI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        bossG = this.GetComponent<GameObject>();
         cam = Camera.main;
         camPosX = cam.transform.position.x;
         camPosY = cam.transform.position.y;
         camHeigth = 2f * cam.orthographicSize;
         camWidth = camHeigth * cam.aspect;
         camdiv3 = camWidth / 3;
-        //Debug.Log(camHeigth);
+        camPosXCurent = camPosX - camWidth / 2;
+        camPosYCurent = camPosY + camHeigth / 2;
+        //Debug.Log(camPosYCurent);
         //Debug.Log(camWidth);
-        //Debug.Log(camdiv3);
-
+        //Debug.Log(camPosXCurent);
+        bossSize = 5;
+        //Debug.Log(bossSize);
         safeAreaSize = safeArea.transform.localScale.x;
         safeAreaRadius = safeAreaSize / 2;
         //Debug.Log(safeAreaSize);
         //Debug.Log(safeAreaRadius);
-        SafeAreaSpawn();
+        //SafeAreaSpawn();
     }
 
     // Update is called once per frame
@@ -81,7 +102,7 @@ public class phaseI : MonoBehaviour
         if (shootTimer >= 5)
         {
             phaseION = true;
-            //ShotShockWave();
+            ShotShockWave();
         }
         
     }
@@ -90,6 +111,7 @@ public class phaseI : MonoBehaviour
     {
         phaseITimer = phaseITimer + Time.deltaTime;
         Debug.Log(phaseITimer);
+        SafeAreaSpawn();
         Vector3 Shotdirection = new Vector3(0, -1, 0);
         if ((bulletShootTime <= 0))
         {
@@ -111,15 +133,40 @@ public class phaseI : MonoBehaviour
 
     private void SafeAreaSpawn()
     {
-        rect1moreX = camdiv3 - safeAreaRadius;
-        Debug.Log(rect1moreX);
-        Instantiate(safeArea, new Vector3(rect1moreX, 0, 0), Quaternion.identity);
+        
+        if (spawnAreaBool == false)
+        {
+            
+            //rect1
+            rect1moreX = camPosXCurent + camdiv3 - safeAreaRadius;
+            rect1lessX = camPosXCurent + safeAreaRadius;
+            rect1moreY = camPosYCurent - camHeigth + safeAreaRadius;
+            rect1lessY = camPosYCurent - bossSize - 2 - safeAreaRadius;
+            //Debug.Log(rect1moreX);
+            Instantiate(safeArea, new Vector3(Random.Range(rect1moreX, rect1lessX), Random.Range(rect1moreY, rect1lessY), 0), Quaternion.identity);
+            //Instantiate(safeArea, new Vector3(rect1moreX, 0, 0), Quaternion.identity);
+
+
+            //rect2
+            rect2moreX = camPosXCurent + camdiv3 * 2 - safeAreaRadius;
+            rect2lessX = camPosXCurent + camdiv3 + safeAreaRadius;
+            rect2moreY = camPosYCurent - camHeigth + safeAreaRadius;
+            rect2lessY = camPosYCurent - bossSize - 2 - safeAreaRadius;
+            Instantiate(safeArea, new Vector3(Random.Range(rect2moreX, rect2lessX), Random.Range(rect2moreY, rect2lessY), 0), Quaternion.identity);
+
+            //rect3
+            rect3moreX = camPosXCurent + camdiv3 * 3 - safeAreaRadius;
+            rect3lessX = camPosXCurent + camdiv3 * 2 + safeAreaRadius;
+            rect3moreY = camPosYCurent - camHeigth + safeAreaRadius;
+            rect3lessY = camPosYCurent - bossSize - 2 - safeAreaRadius;
+            Instantiate(safeArea, new Vector3(Random.Range(rect3moreX, rect3lessX), Random.Range(rect3moreY, rect3lessY), 0), Quaternion.identity);
+            spawnAreaBool = true;
+        }
+        
+
     }
 
-    private void RandomNumberGenerator()
-    {
-
-    }
+    
 
     
 }
