3232a7e085978d7f85c03ba3de84143c5cbba6d8
HugoBonito <92577431+HugoBonito@users.noreply.github.com>
2022-01-06 17:47:29 +0000

    Boss Health

--- Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI/ShockWave.cs
+++ Miners Of The Void/Assets/Scripts/Enemies/Boss/phaseI/ShockWave.cs
@@ -7,10 +7,23 @@ public class ShockWave : MonoBehaviour
 
     public float bulletLifeTime = 3;
 
-    
+    //cam
+    private Camera cam;
+    private float camHeigth;
+    private float camWidth;
+
+
     void Start()
     {
+
+        cam = Camera.main;
+        camHeigth = 2f * cam.orthographicSize;
+        camWidth = camHeigth * cam.aspect;
+
+        transform.localScale = new Vector3(camWidth, 0.5f, 0);
+
         Destroy(gameObject, bulletLifeTime);
     }
+
 
 }
